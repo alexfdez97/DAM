b28d122abf87c7417df56458b524a2840efa7027
alejandrofm <fdezalejandro97@gmail.com>
2018-05-28 16:50:29 +0200

    Ejer8 v0.01

---
+++ Ejer8/src/ejer4bol8/Ejer4Bol8.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer4bol8;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author alex
+ */
+public class Ejer4Bol8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormEjer8 form = new FormEjer8();
+
+        Dimension resolucion = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension resolucionMinima = resolucion;
+//        double width = resolucion.getWidth();
+//        double height = resolucion.getHeight();
+        int ancho = 210;
+        int alto = 300;
+        form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        form.setSize(ancho, alto);
+        form.setLocationRelativeTo(null);
+        form.setVisible(true);
+        resolucionMinima.setSize(ancho, alto);
+        form.setMinimumSize(resolucionMinima);
+    }
+    
+}
---
+++ Ejer8/src/ejer4bol8/FormEjer8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer4bol8;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author alex
+ */
+public class FormEjer8 extends JFrame {
+
+    JCheckBox chkCajas[];
+    
+    public FormEjer8() {
+    }
+    
+    
+}
